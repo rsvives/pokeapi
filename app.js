bc9fd4bd447eb6268e5f2a097090ed7b1ff8f4df
rodri-afa <rodrigo.afaformacion@gmail.com>
2024-07-26 18:14:11 +0200

    bootstrap

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 async function pedirPokemon() {
     const data = await fetch('https://pokeapi.co/api/v2/pokemon/')
     const json = await data.json()
-    console.log(json)
+    // console.log(json)
 
-    // const pokemon = json.results
+    const pokemon = json.results
 
-    // for (const p of pokemon) {
-    //     console.log(p.name)
-    // }
+    const pokemonList = document.getElementById('pokemon-list')
+
+    for (const p of pokemon) {
+        // console.log(p.name)
+        // crear div
+        const pokemonDiv = document.createElement('div')
+        pokemonDiv.className = 'pokemon'
+        // meter dentro el nombre (con un h3)
+        pokemonDiv.innerHTML = `<h3>${p.name}</h3>`
+        // añadir al pokemon-list
+        pokemonList.append(pokemonDiv)
+    }
+
+
 }
 
 
